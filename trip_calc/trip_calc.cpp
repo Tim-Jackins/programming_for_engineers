568b48b943f9d0a45c1f366ba0391b16bf1a75a6
Tim-Jackins <jack@jacktimmmins.com>
2019-09-23 23:59:22 -0400

    Started assignment

---
+++ trip_calc/trip_calc.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <stdint.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    double test = 2.0;
+    cout << test;
+    cin >> test;
+    cout << endl << test;
+
+    int daysSpent;
+    int dayCount = 1;
+    cout << "How many days will you be on the trip? ";
+    cin >> daysSpent;
+
+    leaveTimeRaw
+    
+    "What time will you leave (to the nearest hour)? "
+    "AM (a) or PM (p)? "
+    "What time will you arrive home (to the nearest hour)? "
+    "AM (a) or PM (p)? "
+
+    return 0;
+}
+/*
+The total number of days spent on the trip
+• The time of departure on the first day of the trip, and the time of arrival back home on the last
+day of the trip (to the nearest hour)
+• The amount of any round-trip airfare
+
+• The amount of any car rentals
+• Miles driven, if a private vehicle was used. Calculate the vehicle expense as $0.27 per mile
+driven
+
+• Parking fees (The company allows up to $6 per day. Anything in excess of this must be paid by
+the employee.)
+
+• Taxi fees, if a taxi was used anytime during the trip (The company allows up to $10 per day, for
+each day a taxi was used. Anything in excess of this must be paid by the employee.)
+
+• Hotel expenses (The company allows up to $90 per night for lodging. Anything in excess of this
+must be paid by the employee.)
+• The amount of each meal eaten. On the first day of the trip, breakfast is allowed as an expense if the time of departure is before 7 a.m. Lunch is allowed if the time of departure is before 12 noon. Dinner is allowed on the first day if the time of departure is before 6 p.m. On the last day of the trip, breakfast is allowed if the time of arrival is after 8 a.m. Lunch is allowed if the time of arrival is after 1 p.m. Dinner is allowed on the last day if the time of arrival is after 7 p.m. The program should only ask for the amounts of allowable meals. (The company allows up to $9 for breakfast, $12 for lunch, and $16 for dinner. Anything in excess of this must be paid by the
+employee.)
+
+The program should calculate and display the total expenses incurred by the businessperson, the total allowable expenses for the trip, the excess that must be reimbursed by the businessperson, if any, and the amount saved by the businessperson if the expenses were under the total allowed.
+
+"What is the cost of airfare? "
+"What is the total cost for hotels? "
+
+"Did you drive your own car (y/n)? "
+"y"
+"How many miles did you drive? "
+"n"
+"What is the cost of car rentals? "
+
+cout << "For day " << 1 << ", what were your parking fees? "
+cout << "For day " << 1 << ", what were your taxi fees? "
+"How much was was breakfast? "
+"How much was lunch? "
+"Total expenses incurred by traveller: "
+"Maximum allowable expenses: "
+
+"You saved the company "
+"You must reimburse the company for the " << "coverages."
+*/
